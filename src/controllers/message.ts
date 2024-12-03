dfc469eb4fbc6b90fddd368ccdfabeeb25d034bb
avtandil <shainidze@gmail.com>
2024-12-03 23:51:14 +0200

    add try/catch to message controller

--- src/controllers/message.ts
+++ src/controllers/message.ts
@@ -1,10 +1,14 @@
 import bot from '@providers/bot';
 import { generateText } from '@services/generates';
 bot.on('message', async (ctx) => {
-  const message = ctx.message.text || '';
-  const result = await generateText(message);
+  try {
+    const message = ctx.message.text || '';
+    const result = await generateText(message);
 
-  ctx.reply(result.text, {
-    parse_mode: 'Markdown',
-  });
+    await ctx.reply(result.text, {
+      parse_mode: 'Markdown',
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
